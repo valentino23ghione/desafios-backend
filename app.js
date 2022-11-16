c776ca855b413a73e2212b2a8de0a9bf1f98c421
valentino <valenghionedm@hotmail.com>
2022-11-15 23:45:51 -0300

    server express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require ("path");
+const app = express();
+
+app.get("/", (req, res) => {
+    //res.send("hola mundo!");
+    res.sendFile( path.join(__dirname + "/index.html"))
+});
+
+app.listen(3000, ()=>{
+    console.log("servidor funcionando en puerto", 3000);
+});
